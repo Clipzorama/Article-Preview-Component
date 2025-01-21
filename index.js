e7697270f36debe4d73a49c2da00608f07d085b3
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-01-21 16:42:23 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ const iconPath = document.querySelector(".iconer path");
 
 
 iconContainer.addEventListener("click", () => {
-    if(iconPath.getAttribute("fill") === "#6E8098") {
+
+    if(iconPath.getAttribute("fill") === "#6E8098" && window.innerWidth < 1024) {
         writerDiv.style.backgroundColor = "var(--Very-Dark-Grayish-Blue)";
         iconContainer.style.backgroundColor = "var(--Desaturated-Dark-Blue)";
         iconPath.setAttribute("fill", "var(--Light-Grayish-Blue)");
         profile.classList.add("hidden");
         socials.classList.remove("hidden");
-    } else {
+    } else if (iconPath.getAttribute("fill") !== "#6E8098" && window.innerWidth < 1024) {
         writerDiv.style.backgroundColor = "white";
         iconContainer.style.backgroundColor = "var(--Light-Grayish-Blue)";
         iconPath.setAttribute("fill", "#6E8098");
         profile.classList.remove("hidden");
         socials.classList.add("hidden");
+    } else if (iconPath.getAttribute("fill") === "#6E8098" && window.innerWidth > 1024) {
+        iconContainer.style.backgroundColor = "var(--Very-Dark-Grayish-Blue)";
+        iconPath.setAttribute("fill", "var(--Light-Grayish-Blue)");
+    } else {
+        iconPath.setAttribute("fill", "#6E8098");
+        iconContainer.style.backgroundColor = "var(--Light-Grayish-Blue)";
+
     }
 });
